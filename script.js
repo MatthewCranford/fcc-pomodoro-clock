b24dafd119d2acfe7253b859154526d88f704df2
Matthew Cranford <mcranfordmail@gmail.com>
2018-05-13 17:46:39 -0700

    More work on timer

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
+let flag;
+
 $('#form').on('submit', event => {
   event.preventDefault();
+
+  // if (flag) {
+  //   clearInterval(timer);
+  //   flag = false;
+  //   console.log('Cleared');
+  // }
+
   function initializeTimer() {
     let totalSeconds = parseInt($('#session').val() * 60);
     const breakTime = $('#break').val();
-
     displayTime(totalSeconds);
     startTimer(totalSeconds);
   }
@@ -11,18 +19,19 @@ $('#form').on('submit', event => {
   function displayTime(totalSeconds) {
     minutes = Math.floor(totalSeconds / 60).toString();
     seconds = (totalSeconds % 60).toString();
-
     if (seconds.length < 2) {
       seconds = '0' + seconds;
     }
-
     $('#time').text(`${minutes}:${seconds}`);
   }
 
   function startTimer(totalSeconds) {
+    if (flag) {
+      clearInterval(timer);
+    }
     const timer = setInterval(decrementTime, 1000);
-
     function decrementTime() {
+      flag = true;
       if (totalSeconds === 0) {
         clearInterval(timer);
       }
@@ -31,6 +40,6 @@ $('#form').on('submit', event => {
       displayTime(totalSeconds);
     }
   }
-  reset;
+
   initializeTimer();
 });
